c4cbf990189e9ca42be50d2f80eab3b9aff3d33d
salviotonon <salvio.tonon@gmail.com>
2022-11-25 20:23:22 -0300

    create button menu

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,4 +1,4 @@
-import { Gi3DStairs, GiHamburgerMenu } from 'react-icons/gi'
+import { Gi3DStairs, GiHamburgerMenu, GiExpander } from 'react-icons/gi'
 import { useState } from 'react';
 
 export function Header() {
@@ -7,24 +7,35 @@ export function Header() {
   const handleVisibleMenu = function () {
     setIsVisible(true)
   }
+  const handleCloseMenu = function () {
+    setIsVisible(false)
+  }
   console.log(visible)
   return (
-    <header className='bg-red-600 p-6 flex justify-between mb-11 mt-2'>
+    <header className=' p-6 flex justify-between mb-11 mt-2'>
       <div className='flex items-center justify-start'>
         <Gi3DStairs />
-        <h1 className='font-semibold text-lg ml-1'>Tax<strong className='text-blue'>Pal</strong></h1>
+        <h1 className={`font-semibold text-lg ml-1`}>Tax<strong className='text-blue'>Pal</strong></h1>
       </div>
       <nav className='flex items-center relative'>
-        <div className={`${visible ? 'block' : 'hidden'} absolute p-8 top-20 right-2 w-80 bg-blue`}>
+        <div class={`${visible ? 'block' : 'hidden'} fixed inset-0 bg-slate-300/80  opacity-100`}></div>
+        <div className={`${visible ? 'block' : 'hidden'} absolute p-8 top-20 right-1 w-80 bg-white rounded-xl`}>
+          <GiExpander
+            className={`${visible ? 'block' : 'hidden'} absolute z-20 top-5 right-5`}
+            onClick={handleCloseMenu}
+          />
           <ul >
-            <li>Features</li>
-            <li>Testimonials</li>
-            <li>Pricing</li>
-            <li>Sign in</li>
+            <li className='list-primary'>Features</li>
+            <li className='list-primary'>Testimonials</li>
+            <li className='list-primary'>Pricing</li>
+            <li className='list-primary'>Sign in</li>
           </ul>
         </div>
         <button className='bg-blue text-white px-4 py-2.5 rounded-full text-sm'>Get started</button>
-        <button className='ml-4'><GiHamburgerMenu size={23} onClick={handleVisibleMenu} /></button>
+        <button className='ml-4'>
+          <GiHamburgerMenu size={23} onClick={handleVisibleMenu} className={`${visible ? 'invisible' : 'block'}`} />
+
+        </button>
       </nav>
     </header>
   );
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -9,22 +9,25 @@ module.exports = {
     './src/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    colors: {
-      transparent: 'transparent',
-      current: 'currentColor',
-      'blue': '#2563EB',
-      'white': '#FAFAFA',
-      'black': '#0F172A',
-      'gay': '#334155',
-      'cyan': '#06b6d4',
-      'blueSky': '#3b82f6',
-      'grayText': '#DBEAFE',
-    },
+
     fontFamily: {
       'sans': ['Lexend', 'sans-serif']
 
     },
-    extend: {},
+    extend: {
+      colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        'blue': '#2563EB',
+        'white': '#FFFF',
+        'black': '#0F172A',
+        'gay': '#334155',
+        'cyan': '#06b6d4',
+        'blueSky': '#3b82f6',
+        'grayText': '#DBEAFE',
+        'textMenu': '#0F172A',
+      },
+    },
   },
   plugins: [],
 };
